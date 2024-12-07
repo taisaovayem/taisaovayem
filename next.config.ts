1faffba41d4b8da8465a821d2bfe82dcf4e3fd3c
Thân <minhthan007@gmail.com>
2024-12-07 19:09:49 +0700

    (hotfix) url static file

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   distDir: 'docs',
+  assetPrefix: '/docs',
   rewrites() {
     return Promise.resolve([
       { source: '/docs/_next/:path*', destination: '/_next/:path*' }
