6d1757a58692513545379a568720b164d9063529
Thân <minhthan007@gmail.com>
2024-12-12 23:42:02 +0700

    (feat) dark mode auto

--- src/components/CommonLayout.tsx
+++ src/components/CommonLayout.tsx
@@ -1,7 +1,8 @@
+
 import { Footer } from "@/components";
 import * as React from "react";
 import { Theme, Container } from "@radix-ui/themes";
-
+import { ThemeProvider } from "next-themes";
 
 export function CommontLayout({
   children,
@@ -9,11 +10,13 @@ export function CommontLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <Theme>
-      <Container size="4" className="p-4 xl:p-0">
-        <main className="pt-6">{children}</main>
-        <Footer />
-      </Container>
-    </Theme>
+    <ThemeProvider attribute="class">
+      <Theme>
+        <Container size="4" className="p-4 xl:p-0">
+          <main className="pt-6">{children}</main>
+          <Footer />
+        </Container>
+      </Theme>
+    </ThemeProvider>
   );
 }
