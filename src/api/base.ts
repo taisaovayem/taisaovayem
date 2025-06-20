d38a91ec2e13140e2ad332072b95fa021b770856
Thân <minhthan007@gmail.com>
2025-06-20 22:10:46 +0700

    fix: axios base url

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process?.env?.API ?? "http://taisaovayem.com",
+    baseURL: process?.env?.API ?? "https://admin.taisaovayem.com/wp-json/wp/v2",
     headers: {
         "Content-Type": "application/json",
     },
