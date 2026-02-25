cf243b3c5cca8311d539c4d878c3d120ada2eb79
taisaovayem <why@taisaovayem.com>
2026-02-25 23:02:42 +0700

    feat: add rate limit

---
+++ src/proxy.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const rateLimitMap = new Map<
+  string,
+  { count: number; timestamp: number }
+>();
+
+const WINDOW_SIZE = 60 * 1000; // 1 phút
+const MAX_REQUESTS = 100; // 100 request / phút
+
+export function proxy(request: NextRequest) {
+  const ip =
+    request.headers.get("cf-connecting-ip") ??
+    request.headers.get("x-forwarded-for") ??
+    "anonymous";
+
+  const now = Date.now();
+
+  const data = rateLimitMap.get(ip);
+
+  if (!data) {
+    rateLimitMap.set(ip, { count: 1, timestamp: now });
+  } else {
+    if (now - data.timestamp < WINDOW_SIZE) {
+      data.count += 1;
+
+      if (data.count > MAX_REQUESTS) {
+        return new NextResponse("Too Many Requests", { status: 429 });
+      }
+    } else {
+      rateLimitMap.set(ip, { count: 1, timestamp: now });
+    }
+  }
+console.log(`IP: ${ip}, Count: ${rateLimitMap.get(ip)?.count}`);
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/:path*",
+};
